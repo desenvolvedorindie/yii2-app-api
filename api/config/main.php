c7976230d03d158848cc006e687a3540a3332e88
Welsiton Ferreira <wfcreations@gmail.com>
2016-03-25 03:25:28 -0300

    remove duplicated bootstrap

--- api/config/main.php
+++ api/config/main.php
@@ -11,7 +11,6 @@ return [
     'id' => 'app-api',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'api\controllers',
-    'bootstrap' => ['log'],
     'defaultRoute' => 'v1/default',
     'modules' => [
         'v1' => [
@@ -20,6 +19,7 @@ return [
         ]
     ],
     'bootstrap' => [
+    	'log',
         [
             'class' => \yii\filters\ContentNegotiator::className(),
             'formats' => [
