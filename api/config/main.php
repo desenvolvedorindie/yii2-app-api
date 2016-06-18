08d7f6e8e405b510472fb603cb741084122fc05a
Welsiton Ferreira <wfcreations@gmail.com>
2016-06-18 06:19:22 -0300

    Add user to api

--- api/config/main.php
+++ api/config/main.php
@@ -29,6 +29,10 @@ return [
         ],
     ],
     'components' => [
+    	'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
